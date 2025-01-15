f3e02bfb62999deff591eecad08692808b7793af
Michael Herring <mherring@CHLMichlHerring.home>
2025-01-15 14:29:45 -0600

    first commit

---
+++ config.js
@@ -0,0 +1,15 @@
+
+module.exports = {
+    staging: {
+      baseUrl: "https://live-web-emporia.pantheonsite.io",
+      urls: [
+        "/"
+      ] 
+    },
+    prod: {
+      baseUrl: "https://online.emporia.edu",
+      urls: [
+        "/"
+      ]
+    }
+  };
